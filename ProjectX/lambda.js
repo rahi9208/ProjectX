952912cf3f25d261f141d291b49433b90823d066
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 11:51:59 +0530

    added uuid

--- ProjectX/lambda.js
+++ ProjectX/lambda.js
@@ -1,9 +1,10 @@
 let AWS = require('aws-sdk');
 let mathjs = require('mathjs');
+let uuid = require("uuid");
 exports.handler = function (event, context, callback) {
 
 
 
 
-	callback(null, 'Successfully executed CHathuraXYZDo' + mathjs.pow(2, 4));
+	callback(null, 'Successfully executed CHathuraXYZDo' + mathjs.pow(2, 4) + uuid.v4());
 }
