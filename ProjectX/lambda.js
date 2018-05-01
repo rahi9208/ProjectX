dfbbf89555293ad0ebd61c0ce004a18f25e8439e
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 11:49:17 +0530

    init comm

--- ProjectX/lambda.js
+++ ProjectX/lambda.js
@@ -5,5 +5,5 @@ exports.handler = function (event, context, callback) {
 
 
 
-	callback(null, 'Successfully executed CHathuraXYZ' + mathjs.pow(2, 4));
+	callback(null, 'Successfully executed CHathuraXYZDo' + mathjs.pow(2, 4));
 }
