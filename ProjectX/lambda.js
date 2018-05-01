234d8fc013b8d61804493eeed866137abda5aa3e
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 12:25:25 +0530

    cs

--- ProjectX/lambda.js
+++ ProjectX/lambda.js
@@ -2,10 +2,12 @@ let AWS = require('aws-sdk');
 let mathjs = require('mathjs');
 let uuid = require("uuid");
 var loremIpsum = require('lorem-ipsum');
+var casual = require('casual');
 exports.handler = function (event, context, callback) {
 
 
 
 
-	callback(null, 'Successfully executed CHathuraXYZDoX' + mathjs.pow(2, 4) + uuid.v4() + loremIpsum());
+	callback(null, 'Successfully executed CHathuraXYZDoX'
+		+ mathjs.pow(2, 4) + uuid.v4() + loremIpsum() + casual.sentence);
 }
