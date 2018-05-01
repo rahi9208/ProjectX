8fae262cdd617ce1e5070aafd65679c91d0155b6
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 12:19:42 +0530

    added lorem ipsum

--- ProjectX/lambda.js
+++ ProjectX/lambda.js
@@ -1,10 +1,11 @@
 let AWS = require('aws-sdk');
 let mathjs = require('mathjs');
 let uuid = require("uuid");
+var loremIpsum = require('lorem-ipsum');
 exports.handler = function (event, context, callback) {
 
 
 
 
-	callback(null, 'Successfully executed CHathuraXYZDoX' + mathjs.pow(2, 4) + uuid.v4());
+	callback(null, 'Successfully executed CHathuraXYZDoX' + mathjs.pow(2, 4) + uuid.v4() + loremIpsum());
 }
